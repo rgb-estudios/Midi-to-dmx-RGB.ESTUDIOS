13279c7726346368f0351ee4909c070cf2f0bbc1
rgb-estudios <claudio@rgb-estudios.com>
2026-08-05 04:22:28 -0400

    vst3: add generated version bridge

---
+++ plugins/vst3/proof/version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "pluginterfaces/base/fplatform.h"
+#include "projectversion.h"
+
+#define stringOriginalFilename "AeylaVisualDmxAlpha.vst3"
+#if SMTG_PLATFORM_64
+#define stringFileDescription "AEYLA Visual DMX Alpha VST3 (64Bit)"
+#else
+#define stringFileDescription "AEYLA Visual DMX Alpha VST3"
+#endif
+#define stringCompanyName "RGB Estudios\0"
+#define stringLegalCopyright "Copyright(c) 2026 RGB Estudios."
+#define stringLegalTrademarks "VST is a trademark of Steinberg Media Technologies GmbH"
